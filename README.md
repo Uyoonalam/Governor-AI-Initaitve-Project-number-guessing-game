6eecd356f2c75b29fdd9b4b4ab46578b8e1a0fea
[Uyoon Alam] <[uyoonalam@gmail,com]>
2024-03-23 00:29:05 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# governor-AI-Initaitve-Project-number-guessing-game 
---
+++ index.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const RNG = Math.floor(Math.random() * 10);
+
+interface ResultType {
+  user: number;
+}
+
+async function main() {
+  const result: ResultType = await inquirer.prompt([
+    {
+      type: 'number',
+      name: 'user',
+      message: 'Guess the number between 1 and 10:'
+    }
+  ]);
+
+  const { user } = result;
+
+  if (user === RNG) {
+    console.log(`User guessed number is ${user}.`);
+    console.log(`The system has chosen the number ${RNG}.`);
+    console.log('You have picked the right number.');
+  } else {
+      console.log(`The system has chosen the number ${RNG}.`)
+    console.log('Sorry, please try again.');
+  }
+}
+
+main();
